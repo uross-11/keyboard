a1576e5a7d7c3895dc651bf34a3a34b84bf476c9
uros <u.ignjatovic00@gmail.com>
2023-06-20 11:02:05 +0200

    feat: add sessions, import crypto lib

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { SessionStore } from "./sessionStore.js";
+import crypto from "crypto";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
+const sessionStore = new SessionStore();
+
+const randomId = () => crypto.randomBytes(8).toString("hex");
+
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    // find existing session
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      socket.userID = session.userID;
+      socket.username = session.username;
+      return next();
+    }
+  }
+  // const username = socket.handshake.auth.username;
+  // console.log(username);
+  // if (!username) {
+  //   return next(new Error("invalid username"));
+  // }
+  // create new session
+  socket.sessionID = randomId();
+  socket.userID = randomId();
+  // socket.username = username;
+  next();
+});
 
 io.on("connection", (socket) => {
-  console.log(`connect ${socket.id}`);
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+  console.log(`CONNECT ==> ${socket.id}/${socket.sessionID}/${socket.userID}`);
+  // console.log(`CONNECT ==> ${socket.id}`)
 
   socket.on("ping", (cb) => {
     console.log("ping");
@@ -15,8 +49,10 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log(`disconnect ${socket.id}`);
+    console.log(`DISCONNECT ==> ${socket.id}`);
   });
 });
 
-httpServer.listen(3000);
+httpServer.listen(3000, () => {
+  console.log("server listening at http://localhost:3000");
+});
