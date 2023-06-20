4b3371e37042ca1dde3e4e675f700f9455c4ed33
uross-11 <u.ignjatovic00@gmail.com>
2023-06-20 13:57:38 +0200

    feat: username

--- index.js
+++ index.js
@@ -23,33 +23,56 @@ io.use((socket, next) => {
       return next();
     }
   }
-  // const username = socket.handshake.auth.username;
-  // console.log(username);
-  // if (!username) {
-  //   return next(new Error("invalid username"));
-  // }
+  const username = socket.handshake.auth.username;
+  console.log(username);
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
   // create new session
   socket.sessionID = randomId();
   socket.userID = randomId();
-  // socket.username = username;
+  socket.username = username;
   next();
 });
 
 io.on("connection", (socket) => {
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true,
+  });
+
+  // emit session details
   socket.emit("session", {
     sessionID: socket.sessionID,
     userID: socket.userID,
   });
-  console.log(`CONNECT ==> ${socket.id}/${socket.sessionID}/${socket.userID}`);
+  console.log(`CONNECT ==> ${socket.id}`);
   // console.log(`CONNECT ==> ${socket.id}`)
+  console.log(sessionStore.findAllSessions());
 
-  socket.on("ping", (cb) => {
-    console.log("ping");
+  socket.on("tick", (cb) => {
+    socket.emit("red");
     cb();
   });
 
-  socket.on("disconnect", () => {
-    console.log(`DISCONNECT ==> ${socket.id}`);
+  socket.on("disconnect", async () => {
+    const matchingSockets = await io.in(socket.userID).allSockets();
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // notify other users
+      socket.broadcast.emit("user disconnected", socket.userID);
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false,
+      });
+      console.log(
+        "user disconnected: ",
+        sessionStore.findSession(socket.sessionID)
+      );
+    }
   });
 });
 
