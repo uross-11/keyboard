94b00111f288c0de68e473d6f9b4f72659c4bc51
uros <u.ignjatovic00@gmail.com>
2023-06-28 01:46:17 +0200

    feat: initial design

--- src/client/main.js
+++ src/client/main.js
@@ -1,10 +1,54 @@
 import "./style.css";
 import { setColor } from "./color.js";
+import keyboardBlack from "./keyboard-black.svg";
+import keyboardWhite from "./keyboard-white.svg";
+
+const colors = new Map([
+  ["red", "bg-red-600"],
+  ["green", "bg-green-700"],
+  ["blue", "bg-blue-800"],
+]);
+
+let kbd1 = { color: colors.get("red"), connected: true };
+let kbd2 = { color: colors.get("red"), connected: true };
 
 document.querySelector("#app").innerHTML = `
-  <main>
-    <div class="card">
-      <button id="red" type="button">Red</button>
+  <main class="h-full text-white px-5 pt-7 lg:flex lg:gap-11 lg:justify-center lg:mt-40">
+    <div class="${kbd1.connected ? "" : "opacity-30"} h-80 w-full border border-zinc-600 rounded-lg mb-7 relative p-5 grid lg:max-w-lg">
+      <span class="absolute flex justify-center top-[-1.25rem] left-4 bg-zinc-900 text-3xl w-9">1</span>
+      <div class="flex justify-end h-fit">
+        <span class="relative flex h-3 w-3">
+          <span class="${kbd1.connected ? "animate-ping bg-green-500" : "bg-zinc-500"} absolute inline-flex h-full w-full rounded-full opacity-75"></span>
+          <span class="${kbd1.connected ? "bg-green-500" : "bg-zinc-500"} relative inline-flex rounded-full h-3 w-3"></span>
+        </span>
+      </div>
+      <div class="w-72 mx-auto h-fit relative">
+        <div class="${kbd1.connected ? kbd1.color : ""} inset-[-7px] absolute -z-10 blur-sm"></div>
+        <img src="${keyboardWhite}" class="opacity-90" alt="Keyboard 1 image">
+      </div>
+      <div class="flex gap-4 mx-auto self-end">
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Red</button>
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Green</button>
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Blue</button>
+      </div>
+    </div>
+    <div class="${kbd2.connected ? "" : "opacity-30"} h-80 w-full border border-zinc-600 rounded-lg relative p-5 grid lg:max-w-lg">
+      <span class="absolute flex justify-center top-[-1.25rem] left-4 bg-zinc-900 text-3xl w-9">2</span>
+      <div class="flex justify-end h-fit">
+        <span class="relative flex h-3 w-3">
+          <span class="${kbd2.connected ? "animate-ping bg-green-500" : "bg-zinc-500"} absolute inline-flex h-full w-full rounded-full opacity-75"></span>
+          <span class="${kbd2.connected ? "bg-green-500" : "bg-zinc-500"} relative inline-flex rounded-full h-3 w-3"></span>
+        </span>
+      </div>
+      <div class="w-72 mx-auto h-fit relative">
+        <div class="${kbd2.connected ? kbd2.color : ""} inset-[-7px] absolute -z-10 blur-sm"></div>
+        <img src="${keyboardBlack}" class="opacity-90" alt="Keyboard 2 image">
+      </div>
+      <div class="flex gap-4 mx-auto self-end">
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Red</button>
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Green</button>
+        <button id="red" class="text-zinc-100 bg-zinc-900 border border-zinc-600 hover:border-zinc-400 active:bg-zinc-800 mx-auto block py-2 px-5 text-md rounded-lg lg:py-1 lg:px-6">Blue</button>
+      </div>
     </div>
   </main>
 `;
