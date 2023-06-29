e6cf54591a265eb8162fd45a3d647ac7f5e30ee2
uros <u.ignjatovic00@gmail.com>
2023-06-29 14:19:54 +0200

    config: change to esm

--- src/server/main.js
+++ src/server/main.js
@@ -1,21 +1,92 @@
-const express = require("express");
-const ViteExpress = require("vite-express");
-const { Server } = require("socket.io");
-const { createServer } = require("http");
+import "dotenv/config.js";
+import express from "express";
+import crypto from "crypto";
+import ViteExpress from "vite-express";
+import { createServer } from "http";
+import { SessionStore } from "./sessionStore.js";
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
+const sessionStore = new SessionStore();
+
+const keyboard1 = process.env.KEYBOARD_1;
+const keyboard2 = process.env.KEYBOARD_2;
+const frontend = process.env.FRONTEND;
+
+console.log(keyboard1, keyboard2, frontend);
+
+const id = ["red-1", "green-1", "blue-1", "red-2", "green-2", "blue-2"];
+
+// crypto deprecated
+const randomId = () => crypto.randomBytes(8).toString("hex");
+
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    // find existing session
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      socket.userID = session.userID;
+      socket.username = session.username;
+      return next();
+    }
+  }
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  // create new session
+  socket.sessionID = randomId();
+  socket.userID = randomId();
+  socket.username = username;
+  next();
+});
 
 io.on("connection", (socket) => {
-  console.log("user connected: ", socket.id);
-
-  socket.on("red", () => {
-    console.log("red");
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true,
   });
 
-  socket.on("disconnect", () => {
-    console.log("user disconnected: ", socket.id);
+  console.log("user connected: ", sessionStore.findSession(socket.sessionID));
+
+  // emit session details
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+
+  socket.on("color", (payload) => {
+    console.log(payload);
+    sessionStore.findSession(socket.sessionID).username;
+  });
+
+  socket.on("tick", (cb) => {
+    socket.emit("red");
+    cb();
+  });
+
+  socket.on("disconnect", async () => {
+    const matchingSockets = await io.in(socket.userID).allSockets();
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // notify other users
+      socket.broadcast.emit("user disconnected", socket.userID);
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        username: socket.username,
+        connected: false,
+      });
+      console.log(
+        "user disconnected: ",
+        sessionStore.findSession(socket.sessionID).username
+      );
+    }
   });
 });
 
@@ -23,9 +94,9 @@ app.get("/hello", (_, res) => {
   res.send("Hello Vite!");
 });
 
-const server =  httpServer.listen(3000, () => {
+const server = httpServer.listen(3000, () => {
+  sessionStore.restoreSessions();
   console.log("server listening at http://localhost:3000");
 });
 
 ViteExpress.bind(app, server);
-
